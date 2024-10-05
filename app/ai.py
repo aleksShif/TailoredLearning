2373b25d65dbb67367a131e1fc09649322cda727
TheAlienSeb <93489617+TheAlienSeb@users.noreply.github.com>
2024-10-05 17:11:14 -0400

    API

---
+++ app/ai.py
@@ -0,0 +1,31 @@
+import os
+from openai import OpenAI
+OpenAI.api_key = ''
+
+def getQuestion(role,prompt):
+    Question = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": f"""{role}"""
+            },
+            {
+                "role": "user",
+                "content": f"""{prompt}"""
+            }
+        ],
+        temperature=.5, #the randomness/ unpredictablitiy of the question
+        max_tokes=40,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response.choices[0].message.content
+
+    Role = "You are a quiz question generator. You create a question with four possible answers."
+    Message = "Can I have a question about C++ data structures and algorithms?"
+
+    print(getQuestion(Role,Message))
+
+    
